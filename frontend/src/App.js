536fb2013f9f57ba2ffc47c37f1df9a35de13a98
ashpakmulanii <ashpakmulani1101@gmail.com>
2022-04-14 08:39:58 +0530

    installed react

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { Footer } from "./Components/Footer";
+import { Header } from "./Components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
