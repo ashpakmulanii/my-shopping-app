2105731ef10de1b773f107585df320252ed17f74
ashpakmulanii <ashpakmulani1101@gmail.com>
2022-04-17 10:44:53 +0530

    created models for products, reviews, orders,order items and shipping. and static file configration has done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_ROOT = 'static/images'
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
